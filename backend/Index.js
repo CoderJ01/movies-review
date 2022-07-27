8b06c0f406f80645a20f82a7442b454e86452dc1
Joshua <buildingguy@sbcglobal.net>
2022-07-26 20:33:02 -0700

    set up server

---
+++ backend/Index.js
@@ -0,0 +1,62 @@
+// middleware
+import express from 'express';
+import cors from 'cors';
+
+// access environment variables
+import dotenv from 'dotenv';
+
+// access database
+import mongodb from 'mongodb';
+
+// file to be created later
+import MoviesRoute from '.api/MoviesRoute.js';
+
+class Index {
+    // create server
+    static app = express();
+
+    // provide access to export router
+    static router = express.Router();
+
+    // define application flow of execution
+    static main() {
+        dotenv.config(); // load environment variables
+        Index.setUpServer();
+        Index.setUpDatabase();
+    }
+
+    // configure server execution
+    // attach cors and express.json middleware
+    static setUpServer() {
+        Index.app.use(cors());
+        Index.app.use(express.json()); // enable server to read and accept JSON in request's body
+        
+        // specify initial routes 
+        Index.app.use('/api/vl/movies',
+            MoviesRoute.configRoutes(Index.Router));
+        Index.app.use('*', (req, res) => {
+            res.status(404).json({error: 'not found'});
+        });
+    }
+
+    static async setUpDatabase() {
+        const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI); // create MongoClient instance and pass
+                                                                                 // it in the database URI
+        const port = process.env.PORT || 8000;
+        try {
+            // Connect to MongoDB cluster
+            await client.connect();
+
+            // start web server
+            Index.app.listen(port, () => {
+                console.log(`server is running on port:${port}`);
+            });
+        } catch (e) {
+            console.log(e);
+            process.exit(1);
+        }
+    }
+}
+
+// initialize application execution
+Index.main();
