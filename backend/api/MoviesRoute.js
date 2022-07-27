78426be5beae7710841e020bffdaeb3a3f54e357
Joshua <buildingguy@sbcglobal.net>
2022-07-27 13:17:35 -0700

    apply controller to route

--- backend/api/MoviesRoute.js
+++ backend/api/MoviesRoute.js
@@ -1,7 +1,11 @@
-// contains different routes to which people can go
+// file contains different routes to which people can go
+
+import MoviesController from './MoviesController.js';
+
 export default class MoviesRoute {
     static configRoutes(router) {
-        router.route('/').get((req, res) => res.send('hello world'));
+        // router.route('/').get((req, res) => res.send('hello world'));
+        router.route('/').get(MoviesController.apiGetMovies); // call MoviesController.apiGetMovies.
         return router;
     }
 }
