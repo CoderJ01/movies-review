886b2e363a33bb07b1bfdd97c0ed9607cef3ab95
Joshua <buildingguy@sbcglobal.net>
2022-07-27 16:36:20 -0700

    methods to add, update, and delete reviews (ReviewsDAO.js)

--- backend/dao/ReviewsDAO.js
+++ backend/dao/ReviewsDAO.js
@@ -17,4 +17,49 @@ export default class ReviewsDAO {
             console.log(`unable to establish connection handle in reviewDAO: ${e}`)
         }
     }
+
+    static async addReview(movieId, user, review, date) {
+        try {
+            const reviewDoc = {
+                name: user.name,
+                user_id: user.id,
+                date,
+                review,
+                movie_id: ReviewsDAO.ObjectId(movieId)
+            }
+            return await ReviewsDAO.reviews.insertOne(reviewDoc);
+        }
+        catch(e) {
+            console.error(`unable to post review: ${e}`);
+            return { error: e };
+        }
+    }
+
+    static async updateReview(reviewId, userId, review, date) {
+        try {
+            const updateResponse = await ReviewsDAO.reviews.updateOne(
+                { user_id: userId, _id: ReviewsDAO.ObjectId(reviewId) },
+                { $set: { review, date } }
+            );
+            return updateResponse;
+        }
+        catch(e) {
+            console.error(`unable to update review: ${e}`);
+            return { error: e }
+        }
+    }
+
+    static async deleteReview(reviewId, userId) {
+        try {
+            const deleteResponse = await ReviewsDAO.reviews.deleteOne({
+                _id: ReviewsDAO.ObjectId(reviewId),
+                user_id: userId
+            });
+            return deleteResponse;
+        }
+        catch(e) {
+            console.log(`unable to delete review: ${e}`);
+            return { error: e }
+        }
+    }
 }
