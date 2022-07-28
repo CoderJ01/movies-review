12c8e6ee843e15a4c9161b48dc182f09302443ae
Joshua <buildingguy@sbcglobal.net>
2022-07-28 15:29:12 -0700

    import HttpClientModule into AppModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MoviesListComponent } from './components/movies-list/movies-list.compon
 import { MovieComponent } from './components/movie/movie.component';
 import { AddReviewComponent } from './components/add-review/add-review.component';
 import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
