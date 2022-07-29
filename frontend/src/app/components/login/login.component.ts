18a22cb77f58121e5ad90fade4977de57b881ef2
Joshua <buildingguy@sbcglobal.net>
2022-07-29 15:28:57 -0700

    save

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -9,19 +9,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  // FormGroup provides login form
+  // set initial values of name and id as empty strings
   form = new FormGroup({
-    name: new FormControl(' '),
+    name: new FormControl(''),
     id: new FormControl('')
   });
 
   constructor(private _loginService: LoginService, private _router: Router) {}
 
+  // called when user clicks on submit button
   login() {
     this._loginService.login(
       this.form.controls['name'].value,
       this.form.controls['id'].value
     );
 
+    // check if login is successful
     if (
       this._loginService.user.name.length > 0 &&
       this._loginService.user.id.length > 0
