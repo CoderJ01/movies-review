d1e76c5cab6d884f2605fe5a59e91d23d05e0587
Joshua <buildingguy@sbcglobal.net>
2022-07-29 15:27:37 -0700

    login component

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form = new FormGroup({
+    name: new FormControl(' '),
+    id: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(private _loginService: LoginService, private _router: Router) {}
+
+  login() {
+    this._loginService.login(
+      this.form.controls['name'].value,
+      this.form.controls['id'].value
+    );
+
+    if (
+      this._loginService.user.name.length > 0 &&
+      this._loginService.user.id.length > 0
+    ) {
+      // route to homepage
+      this._router.navigate(['/']);
+    }
+  }
 
   ngOnInit(): void {
   }
