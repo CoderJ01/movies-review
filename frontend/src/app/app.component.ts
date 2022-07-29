3af3b91f351ac8457928835c2903c7f01016d5b6
Joshua <buildingguy@sbcglobal.net>
2022-07-29 15:44:56 -0700

    import LoginService in AppComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from './services/login.service';
+import User from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  // title = 'frontend';
+  constructor(private _loginService: LoginService) {}
+
+  ngOnInit(): void {}
+
+  // expose LoginServices user object for use in template
+  get userInfo(): User {
+    return this._loginService.user;
+  }
+
+  // call LoginService's logout function
+  logout() {
+    this._loginService.logout();
+  }
 }
