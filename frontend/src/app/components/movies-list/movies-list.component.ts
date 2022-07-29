7cc45b1a918fc60e43cd2d020f589553f8097b26
Joshua <buildingguy@sbcglobal.net>
2022-07-28 17:51:26 -0700

    start MovieList component implementation

--- frontend/src/app/components/movies-list/movies-list.component.ts
+++ frontend/src/app/components/movies-list/movies-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MovieDataService } from 'src/app/services/movie.service';
+import Movie from 'src/app/interfaces/movie';
+import { FormControl } from '@angular/forms';
+import { filter, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
-  styleUrls: ['./movies-list.component.css']
+  // styleUrls: ['./movies-list.component.css']
+  providers: [MovieDataService]
 })
-export class MoviesListComponent implements OnInit {
+export class MoviesListComponent implements OnInit, OnDestroy {
+  title = new FormControl(''); // represents title field
+  ratingsDropdown = new FormControl(); // represents ratings dropdown field
+  
+  movies: Array<Movie> = []; // hold all movies displayed
+  ratings: Array<string> = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  subscriptionRatings!: Subscription;
+  subscriptionMovies!: Subscription;
 }
