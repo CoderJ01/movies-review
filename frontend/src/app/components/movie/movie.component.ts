2bf37a9cccae005be267e4100092ecea948d8fce
Joshua <buildingguy@sbcglobal.net>
2022-07-29 18:30:51 -0700

    psuedocode

--- frontend/src/app/components/movie/movie.component.ts
+++ frontend/src/app/components/movie/movie.component.ts
@@ -62,6 +62,8 @@ export class MovieComponent implements OnInit, OnDestroy {
   deleteReview(reviewId: string) {
     this._movieDataService
       .deleteReview(reviewId, this._loginService.user.id)
+      // filter creates new array will all test-passing elements
+      // new review arrays will not contain reviews marked for deletion
       .subscribe((response) => {
         this.movie.reviews = this.movie.reviews.filter(
           ({ _id }) => _id !== reviewId
